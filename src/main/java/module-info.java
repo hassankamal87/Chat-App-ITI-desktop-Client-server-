c1dfd2eef53ed05220d94ca61526b3722a12dd13
Hassan kamal <118057412+hassankamal87@users.noreply.github.com>
2024-01-05 18:26:24 +0200

    multi module configuration.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chatapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.chatapplication to javafx.fxml;
-    exports com.example.chatapplication;
-}
