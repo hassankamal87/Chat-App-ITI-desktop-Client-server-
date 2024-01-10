cfc5b32c06df5da18c071de924b4ec4079d0b36f
Hassan kamal <118057412+hassankamal87@users.noreply.github.com>
2024-01-10 18:59:41 +0200

    try to solve conflict

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.whisper.client {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-    requires org.jsoup;
 
     opens com.whisper.client to javafx.fxml;
     exports com.whisper.client;
