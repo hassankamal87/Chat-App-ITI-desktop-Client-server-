a256ad7e02528e71fd33544e4fa96640c229cccf
Mosadek <ahmedmosadek3@gmail.com>
2024-01-10 18:43:24 +0200

    added announcment UI

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.whisper.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens com.whisper.client to javafx.fxml;
---
+++ server/src/main/java/com/whisper/server/announcer/controller/AnnouncerHomeController.java
@@ -0,0 +1,19 @@
+package com.whisper.server.announcer.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.HTMLEditor;
+
+public class AnnouncerHomeController
+{
+
+    @FXML
+    private HTMLEditor htmlEditor;
+    @FXML
+    private AnchorPane announcmentPane;
+
+    public void initialize()
+    {
+
+    }
+}
