d7b463666a145623740b5edec6484b2182f5dbef
Mosadek <ahmedmosadek3@gmail.com>
2024-01-19 07:02:37 +0200

    modified the UI

--- server/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.whisper.server {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens com.whisper.server to javafx.fxml;
-    exports com.whisper.server;
-}
