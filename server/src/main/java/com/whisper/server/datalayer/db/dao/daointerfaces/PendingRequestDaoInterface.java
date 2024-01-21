c548a8d020c84b9c8d429efdc4a2404aa55aac9b
Mosadek <ahmedmosadek3@gmail.com>
2024-01-21 19:25:54 +0200

    split the dao

---
+++ server/src/main/java/com/whisper/server/datalayer/db/dao/daointerfaces/PendingRequestDaoInterface.java
@@ -0,0 +1,6 @@
+package com.whisper.server.datalayer.db.dao.daointerfaces;
+
+import com.whisper.server.model.PendingRequest;
+
+public interface PendingRequestDaoInterface extends CrudDaoInterface<PendingRequest>{
+}
---
+++ server/src/main/java/com/whisper/server/datalayer/db/dao/daointerfaces/RoomChatDaoInterface.java
@@ -0,0 +1,6 @@
+package com.whisper.server.datalayer.db.dao.daointerfaces;
+
+import com.whisper.server.model.RoomChat;
+
+public interface RoomChatDaoInterface extends CrudDaoInterface<RoomChat>{
+}
