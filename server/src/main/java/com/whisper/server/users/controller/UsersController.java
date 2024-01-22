4535e115f169bb51cdfa8c6c862e5aae23d77451
Hassan kamal <118057412+hassankamal87@users.noreply.github.com>
2024-01-22 16:27:48 +0200

    Users Table UI Server

---
+++ server/src/main/java/com/whisper/server/users/controller/UsersController.java
@@ -0,0 +1,38 @@
+package com.whisper.server.users.controller;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+public class UsersController {
+
+    @FXML
+    private TableView usersTable;
+    @FXML
+    private TableColumn userNameColumn;
+    @FXML
+    private TableColumn emailColumn;
+    @FXML
+    private TableColumn phoneColumn;
+    @FXML
+    private TableColumn genderColumn;
+    @FXML
+    private TableColumn dobColumn;
+    @FXML
+    private TableColumn countryColumn;
+    @FXML
+    private TableColumn modeColumn;
+    @FXML
+    private TableColumn statusColumn;
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    public void onInputMethodTextChanged(Event event) {
+    }
+}
