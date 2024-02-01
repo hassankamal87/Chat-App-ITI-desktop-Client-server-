95c368b9882a03aaa31a59890f4aaece993063e5
Mosadek <48337574+AhmedMosadek@users.noreply.github.com>
2024-02-01 15:11:50 +0200

    Update HomeServerController.java

--- server/src/main/java/com/whisper/server/presentation/controllers/HomeServerController.java
+++ server/src/main/java/com/whisper/server/presentation/controllers/HomeServerController.java
@@ -1,8 +1,6 @@
 package com.whisper.server.presentation.controllers;
 
 import com.whisper.server.business.services.ServerService;
-import com.whisper.server.persistence.daos.ContactDao;
-import com.whisper.server.persistence.daos.UserDao;
 import com.whisper.server.persistence.db.MyDatabase;
 import com.whisper.server.presentation.services.SceneManager;
 import javafx.application.Platform;
@@ -17,18 +15,6 @@ import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
-import org.example.entities.*;
-import org.example.serverinterfaces.ContactServiceInt;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.sql.Date;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeServerController {
 
@@ -104,11 +90,7 @@ public class HomeServerController {
             toggleSwitch.setDisable(true);
             disableButtons();
             new Thread(() -> {
-                try {
-                    performOperation();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                performOperation();
                 Platform.runLater(() -> {
                     toggleSwitch.setDisable(false);
                     handleToggleSwitchChange(isSwitchOn);
@@ -122,17 +104,17 @@ public class HomeServerController {
                 event -> {
                     handleButtonClick(button1, button2, button3);
                     mainNavigatorPane.setCenter(usersPane);
-        });
+                });
         button2.setOnAction(
                 event -> {
                     handleButtonClick(button2, button1, button3);
                     mainNavigatorPane.setCenter(announcementPane);
-        });
+                });
         button3.setOnAction(
                 event -> {
                     handleButtonClick(button3, button1, button2);
                     mainNavigatorPane.setCenter(statisticsPane);
-        });
+                });
     }
 
     private void handleButtonClick(Button clickedButton, Button otherButton1, Button otherButton2) {
@@ -143,17 +125,15 @@ public class HomeServerController {
     }
 
 
-    private void performOperation() throws IOException {
+    private void performOperation() {
 //        try {
 //            Thread.sleep(1000);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-        Path path = Path.of("E:\\downloads\\test.jpg");
-         Files.readAllBytes(path);
-       User myUser = new User(7,"0111111111","123","email"
+      /*  User myUser = new User(3,"0111111111","123","email"
                 ,"hassan", Gender.male,new Date(System.currentTimeMillis())
-                ,"Algeria","bio", Mode.available, Status.online, Files.readAllBytes(path));
+                ,"Algeria","bio", Mode.avalible, Status.online);*/
 //        serverService serverService=new serverService();
 //        System.out.println(serverService.viewClients().size());
 //        List<User> contacts =new ArrayList<>();
@@ -165,11 +145,7 @@ public class HomeServerController {
 //        }
 //        System.out.println("done"+contacts.size());
 
-        try {
-            System.out.println(UserDao.getInstance(MyDatabase.getInstance()).updateUser(myUser));
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+
     }
 
     private void handleToggleSwitchChange(boolean isSwitchOn) {
