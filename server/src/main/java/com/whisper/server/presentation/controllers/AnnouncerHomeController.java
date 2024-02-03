c65886800121d335314716f37324529123514beb
Mosadek <ahmedmosadek3@gmail.com>
2024-02-03 16:25:53 +0200

    created announcment controller

--- server/src/main/java/com/whisper/server/presentation/controllers/AnnouncerHomeController.java
+++ server/src/main/java/com/whisper/server/presentation/controllers/AnnouncerHomeController.java
@@ -1,5 +1,6 @@
 package com.whisper.server.presentation.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
@@ -14,6 +15,17 @@ public class AnnouncerHomeController
 
     public void initialize()
     {
+
+    }
+
+    public void onAnnounceClicked(ActionEvent actionEvent) {
+        String htmlText = htmlEditor.getHtmlText();
+        htmlEditor.setHtmlText("");
+
+        // the htmlText contains the text with its style
+        // need to pass this string to a web view to display it
+        // look at the RoomChatController class for an example
+        // at method appendMessageInList
 
     }
 }
